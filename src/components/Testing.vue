72fd86718e550b00bc5028c960580f864705b8c8
James Mac <jminerswebb@gmail.com>
2025-08-07 15:10:09 -0700

    add vite

---
+++ src/components/Testing.vue
@@ -0,0 +1,20 @@
+<template>
+<Slider v-model="value" />
+<ColorPicker></ColorPicker>
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import Slider  from 'primevue/colorpicker';
+
+
+const value = ref(0)
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
